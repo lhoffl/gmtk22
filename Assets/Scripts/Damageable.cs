0d02274996c1e78d3a3e54dfa63e79896d4441cd
Matthew Hoffman <matt@lhoffl.com>
2022-07-16 14:14:25 -0400

    fixed bug where paper plane could not die on collision or arrival

--- Assets/Scripts/Damageable.cs
+++ Assets/Scripts/Damageable.cs
@@ -3,7 +3,6 @@ public class Damageable : MonoBehaviour {
     [SerializeField] Health _health;
 
     public void TakeDamage(int amount) {
-        Debug.Log(amount);
         if (_health != null) 
             _health.Remove(Mathf.Abs(amount));
     }
--- Assets/Scripts/PaperPlaneEnemy.cs
+++ Assets/Scripts/PaperPlaneEnemy.cs
@@ -1,11 +1,20 @@
+using System;
+using UnityEngine;
 public class PaperPlaneEnemy : Enemy {
+    [SerializeField] int _damageAmountOnImpact = 3;
+    
     protected override void Update() {
         if (transform.position == _positions [_currentPosition].position) {
-            Destroy(this);
+            GetComponent<Damageable>().TakeDamage(int.MaxValue);
         }
         
         base.Update();
     }
-    
-    
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.GetComponent<PlayerController>() != null) {
+            other.gameObject.GetComponent<Damageable>().TakeDamage(_damageAmountOnImpact);
+            GetComponent<Damageable>().TakeDamage(int.MaxValue);
+        }
+    }
 }
