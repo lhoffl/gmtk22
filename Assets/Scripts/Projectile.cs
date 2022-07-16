b53b75be4ee3d608b543798a1319083c5676735a
Matthew Hoffman <matt@lhoffl.com>
2022-07-16 01:15:11 -0400

    roll sprite for d4 and d6

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
     [SerializeField] float _duration;
     [SerializeField] int _damageAmount;
+    [SerializeField] bool _rotate = true;
+    [SerializeField] float _rotationSpeed = 0.01f;
     
     Rigidbody2D _rigidbody;
     Collider2D _collider;
@@ -16,12 +18,19 @@ public class Projectile : MonoBehaviour {
 
     void Update() {
         _count -= Time.deltaTime;
+        if (_rotate) {
+            transform.Rotate(0f, 0f,  Time.deltaTime * _rotationSpeed);
+        }
+        
         if (_count <= 0) {
             _gun.AddToPool(this);
         }
     }
 
-    void OnEnable() => _count = _duration;
+    void OnEnable() {
+        _count = _duration;
+        transform.localRotation = Quaternion.identity;
+    }
 
     void OnTriggerEnter2D(Collider2D other) {
         if (other == null) return;
